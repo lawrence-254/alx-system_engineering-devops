34907d783b712b584c2dc917575887a86fa1671c
lawrence-254 <lodhiambo26@gmail.com>
2023-09-22 10:26:53 -0700

    2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 #  a manifest that kills a process named killmenow
-exec { 'kill_killmenow_process':
-  command     => 'pkill -9 killmenow',
-  path    => ['/usr/bin', '/usr/sbin'],
+
+exec {'pkill':
+    command => '/usr/bin/pkill killmenow'
 }
