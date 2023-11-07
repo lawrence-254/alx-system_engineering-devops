254be7ed1c3fa3ba8393d2f03d1a6d0fdc6921b0
lawrence-254 <lodhiambo262gmail.com>
2023-11-07 04:07:01 -0800

    2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,32 +8,38 @@ Returns 0 If an invalid subreddit is given
 import requests
 
 
-def top_ten(subreddit):
+def top_ten(subreddit, hot_list=[]):
     """
     Queries a given subredit API and returns number subscribers to the
     subredit
 
     subreddit(str): the name of the subreddit in question
 
-    Returns: Number of sbscribers
+    Returns: Number of sbscribers/hot list
         else 0 if none
     """
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36\
             (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
     headers = {'User-Agent': user_agent}
-    url = f'https://www.reddit.com/r/{subreddit}/new/.json?limit=10'
+    url = f'https://www.reddit.com/r/{subreddit}/new/.json?limit=100'
+    if after is not None:
+        url += '&after' + after
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    subs_count = 0
     if response.status_code == 200:
-        resp_dict = response.json()
-        resp_dict = resp_dict.get('data', {})
+        resps_dict = response.json()
+        resp_dict = resps_dict.get('data', {})
         resp_list = resp_dict.get('children', [])
         for posts in resp_list:
             post = posts.get('data', {})
             title = post.get('title')
             if title is not None:
-                print(title)
+                hot_list.append(title)
+        after = resp_dict.get('after')
+        if after is None:
+            return hot_list
+        else:
+            return (recurse(subreddit, hot_list, after))
     else:
-        print(None)
+        return None
