3b43522d57e85f6555cdec15944a99d191bb834b
lawrence-254 <lodhiambo262gmail.com>
2023-11-07 03:41:18 -0800

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@ Returns: the number of subscribers
 (not active users, total subscribers) for a given subreddit
 Returns 0 If an invalid subreddit is given
 """
-import requeststs
+import requests
 
 
 def number_of_subscribers(subreddit):
@@ -21,11 +21,11 @@ def number_of_subscribers(subreddit):
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36\
             (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
     headers = {'User-Agent': user_agent}
+    url = f'https://www.reddit.com/r/{subreddit}/new/.json'
 
-    response = requests.get('https://www.reddit.com/r/\
-            {subreddit}/new/.json', headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    subs = 0
+    subs_count = 0
     if response.status_code == 200:
         resp_dict = response.json()
         resp_dict = resp_dict.get('data', {})
@@ -33,5 +33,5 @@ def number_of_subscribers(subreddit):
         if len(resp_list) != 0:
             resp_dict = resp_list[0]
             resp_dict = resp_dict.get('data', {})
-            subs = resp_dict.get('subreddit_subscribers', 0)
-    return subs
+            subs_count = resp_dict.get('subreddit_subscribers', 0)
+    return subs_count
